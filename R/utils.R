7ff62edefe385d7dbb06567482c9484331f0974b
Rodrigo Rampazo Amadeu <rramadeu@gmail.com>
2019-06-20 13:37:41 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -529,8 +529,8 @@ mrk_chisq_test<-function(x, m){
   seg.exp <- seg.exp[seg.exp!=0]
   seg.obs <- seg.exp
   seg.obs[names(y)[-length(y)]]<-y[-length(y)]
-  pval <- NA
-  pval <- try(suppressWarnings(stats::chisq.test(x = seg.obs, p = seg.exp[names(seg.obs)])$p.value),silent=TRUE)
+  pval <- catchTry(suppressWarnings(stats::chisq.test(x = seg.obs, p = seg.exp[names(seg.obs)])$p.value),
+                  error = function(e)(return(NA)))
   pval
 }
 
